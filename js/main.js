9d185f589a180aec6831efb5435d3c57ac899d44
josevazquezrmx <jose.vazquez2692@alumnos.udg.mx>
2022-05-26 16:29:50 -0500

    Se borarron lineas de comentarios, ejercicios js

--- js/main.js
+++ js/main.js
@@ -31,9 +31,7 @@ else if ((num3>=num1)&&(num3>=num2)){
     alert (num3+" Es el numero mas grande")
 }
 
-
 //2. Solicitar 3 números (entre el 1 y el 100)  y definir el menor de tres números
-
 
 if ((num1<=num2)&&(num1<=num3)){
     alert (num1+" Es el numero mas pequeño")
@@ -48,8 +46,8 @@ else if ((num3<=num1)&&(num3<=num2)){
 //3. Realizar un algortimo para adivinar un número entre el 1 y el 100 en el menor número de pasos posibles
 
 
-
 //4. Solicitar un entero (entre el 100 y el 200) y determinar si es múltiplo de 3
+
 let num4 = prompt ("Ingresa un numero, entre l00 y 200");
 console.log(num4);
 
@@ -66,6 +64,7 @@ else {
 }
 
 //5. Elaborar un algoritmo para leer 3 números y determinar sí uno es la suma de los otros dos
+
 let num5 = parseInt(prompt ("Ingresa un numero"));
 console.log(num5);
 let num6 = parseInt (prompt ("Ingresa otro numero"));
@@ -87,6 +86,7 @@ else {
 }
 
 //6. Elabora un algoritmo para leer un número y determinar si es par o impar
+
 let num8 = prompt ("Ingresa un numero");
 console.log(num8);
 
@@ -96,84 +96,3 @@ if (num8%2==0){
 else {
     alert (num8+ " No es par");
 }
-
-
-//NO USAR EN PRODUCCION
-// USO EXCLUSIVO DE PRUEBAS
-
-//Mensaje de alerta OK
-
-//alert ("Hola soy un mensaje");
-
-//confirmacion / OK - CANCEL
-
-//confirm ("¿Estas de acuerdo?");
-
-//pregunta - cuadro de texto - ok
-
-//prompt ("Cuál es tu nombre?","");
-
-
-
-// let chilaquiles = confirm("¿Te gustan los chilaquiles verdes?");
-//console.log(chilaquiles);
-
-
-// Operadores de comparación
-// == igual
-// > mayor que
-// >= mayor que
-// < menor que
-// <= menor o igual que
-// != diferente 
-
-
-// if (chilaquiles) {
- //   alert ("Felicidades !!!");
-//} else if  (confirm("¿Y los chilaquiles rojos?")){
-//    alert ("Muy bien!!!");
-//} else { 
-//    alert ("Deberias de probarlos");
-// }
-
-//if Termario
-// (condicion)? verdadero:falso;
-// let mensaje = (chilaquiles)?"felicidades !!!" : "Deberias de probarlos !!!"";
-
-//let mensaje = (chilaquiles)?
-  //                          "felicidades !!!":
-    //                        (confirm("y los chilaquiles rojos?")) "muy bien !!!" :
-      //                      "Deberias de probarlos !!!";
-// alert (mensaje)
-
-//let a = 7;
-//let b = '7';
-//console.log( a==b );
-//console.log( a===b );
-//console.log( a>b );
-//console.log( a>=b );
-//console.log( a<b );
-//console.log( a<=b );
-//console.log( a!=b );
-//console.log( a!==b );
-
-// operadores logicos
-// AND - Y // &&
-// OR - O // ||
-// NOT - No // !
-//console.clear ();
-
-//a = 11;
-//b = 11;
-//c = 12;
-//console.log( (a>b) && (a!=b) );
-
-//console.log( (a>b) || (a!=b) );
-
-//console.log(! (a>b)) ;
-
-//console.log( (a>b) && (a!=b) && (a<c) );
-
-//console.log( (a>b) || (a!=b) && (a<c) );
-
-//console.log( ( (a>b) && (a!=b) ) || (a<c) );
